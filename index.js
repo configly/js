a730561cc38f4ee4f6ccfc28c32ac73e30938e06
Jeremy Smith <jeremy.smith@airbnb.com>
2020-10-18 17:22:28 -0400

    Improve documentation

--- index.js
+++ index.js
@@ -10,46 +10,50 @@ const GET_KEY_IDENTIFIER = 'keys[]';
 
 const GET_API_PATH = '/api/v1/value';
 
-
 /**
  * Config.ly: the dead simple place to put and retrieve your static/config data.
  *
  * Remember: *do NOT* assign the result of a configly.get() to a long-lived variable; in order for
  * the value to fetch from the server, you must call configly.get().
  *
- * Each get(key) return a promise;
+ * Each get(key) returns a Promise; the first argument to the Promise fulfillment method is the
+ * Configly value for the supplied key. Please see the example:
  *
  * const Configly = require('Configly');
  * const configly = new Configly('API_KEY');
  *
  * configly.get('keyOne').then((value) => console.log(value));
+ *
  * // or
+ *
  * const run = async () => {
  *   return await Configly.get('keyOne');
  * }
  *
  * Note that get(key) may or may not make a server request or fetch a cached value. You should
- * assume it'll make a fast HTTP request. If you need something guaranteed to be faster, we
- * recommend storing the value to a local variable; this means you won't receive updates, so
- * be sure to call get() periodically.
+ * assume it'll make a (fast) HTTP request. If you need something guaranteed to be faster, we
+ * recommend storing the value to a local variable; BUT, be aware that this means you won't
+ * receive updates to that variable, so be sure to call get() periodically.
  */
 class Configly {
+
   /**
    * Initialize a new `Configly` with your account's `API Key` and an
    * optional dictionary of `options`.
    *
-   * @param {String} writeKey
+   * @param {String} apiKey - your readonly Config.ly API key. You can find it at
+   *   http://config.ly/config.
    * @param {Object} [options] (optional)
-   *   @property {Number} host (default: https://config.ly/) - Override the host for requests
-   *   @property {Number} enableCache (default: true) - disabling the cache will result in an HTML
-   *   fetch on every `get` call
-   *   @property {Number} timeout (default: 3000) - timeout for request to Configly for data in ms.
+   *     @property {Number} host (default: https://config.ly/) - Overrides the host for requests
+   *     @property {Number} enableCache (default: true) - disables the cache, resulting in an HTTP
+   *       fetch on every `get` call
+   *     @property {Number} timeout (default: 3000) - ms timeout for requests to Configly for data.
    */
   constructor (apiKey, options) {
     options = options || {};
     assert(apiKey, 'You must supply your API Key. You can find it by logging in to Config.ly');
 
-    this.host = removeSlash(options.host || 'https://config.ly');
+    this.host = removeSlash(options.host || 'https://api.config.ly');
     this.cache = {};
     this.cacheTtl = {};
     this.apiKey = apiKey;
@@ -57,7 +61,7 @@ class Configly {
     this.timeout = options.timeout || 3000;
   }
 
-  isCached (key) {
+  _isCached (key) {
     let value = this.cache[key];
     if (!value) {
       return false;
@@ -68,20 +72,22 @@ class Configly {
     return true;
   }
 
-  cacheGet (key) {
+  _cacheGet (key) {
     return this.cache[key];
   }
 
   /**
-   * Fetch values for supplied key. The value may be cached but is still an async call.
+   * Fetch the value for the supplied key. This is an async call; it may be lightning fast as the
+   * value may be cached.
    *
    * @param {String} key - the key to fetch.
-   * @param {Object?} [options] optional to override the global parameters for this request (optional)
-   *   @property {Number} enableCache (default: true) - disabling the cache will result in an HTML
-   *   fetch on every `get` call
-   *   @property {Number} timeout (default: 3000) - timeout for request to Configly for data in ms.
+   * @param {Object?} [options] overrides the global parameters set in the constructor for this
+   *  `get` request only (optional)
+   *     @property {Number} enableCache (default: true) - disables the cache, resulting in an HTTP
+   *       fetch on every `get` call.
+   *     @property {Number} timeout (default: 3000) - timeout for request to Configly for data in ms.
    * @return { Promse<String | Number | Boolean | Array | Object> } returns a promise of the stored
-   * value(s) as typed in Config.ly.
+   *   value(s) as typed in Config.ly.
    */
   get (key, options) {
     assert(typeof key === 'string', 'key must be a string');
@@ -102,8 +108,8 @@ class Configly {
     }
 
     // Check the cache
-    if (cacheIsEnabled && this.isCached(key)) {
-      return Promise.resolve(this.cacheGet(key));
+    if (cacheIsEnabled && this._isCached(key)) {
+      return Promise.resolve(this._cacheGet(key));
     }
 
     let url = `${this.host}${GET_API_PATH}`;
